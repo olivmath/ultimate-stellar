197da27c62c23096ffbf2bbbaa08e2ac9669de29
Lucas Oliveira <olivmath@protonmail.com>
2025-03-22 18:18:45 -0300

    add tests e2e v1, v2

---
+++ test-e2e-v1.sh
@@ -0,0 +1,13 @@
+# CREATE EMPLOYEES WALLETS
+
+# UPLOAD COMPANY: upload Company contract to Stellar Blockchain
+
+# FUND COMPANY: Owner->USDC: approve(paygo, 100)
+
+# CREATE COMPANY: Owner->Paygo: create_company(name, description, list of employees)
+
+# GET accountid OF COMPANY
+
+# CALL PAY FUNCTION OF COMPANY: Owner->Company: call pay function
+
+# VALIDATE PAYMENT TO WALLETS
---
+++ test-e2e-v2.sh
@@ -0,0 +1,9 @@
+# CREATE EMPLOYEES WALLETS
+
+# UPLOAD COMPANY: upload Company contract to Stellar Blockchain
+
+# FUND COMPANY: Owner->USDC: approve(paygo, 100)
+
+# CREATE COMPANY: Owner->Paygo: create_company(name, description, list of employees)
+
+# VALIDATE PAYMENT TO WALLETS
